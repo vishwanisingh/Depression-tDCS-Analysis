b16bb426ec0f26e21fc05fffe0014b175446bc01
vishwanisingh <vishwanisingh@gmail.com>
2024-03-29 15:23:04 +0530

    changes

--- run_all.py
+++ run_all.py
@@ -21,7 +21,7 @@ import papermill as pm
 #     df = pd.DataFrame(columns=columns)
 #     df.to_excel(existing_file, index=True, header=True)
 
-for ACTIVE_SHAM in ['Active']:
+for ACTIVE_SHAM in ['Active', 'Sham']:
     for ELECTRODES in ['32electrodes', '32electrodes-old']:
         if ELECTRODES == '32electrodes':
             SAMPLES = ['Hemlata', 'Malti', 'Preeti', 'Sharifa', 'Vinita', 'VKS']  if ACTIVE_SHAM == 'Active' else ['Geeta', 'Jitendra', 'Jyoti', 'Kuldeep', 'Seema', 'VijayLaxmi']
